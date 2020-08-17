5f26dfefdf858bb8aa3c0d00a2158b450f70f02f
Zeno Popovici <zeno@graffino.com>
2020-08-17 18:06:07 +0300

    Compatibility with future version of WP

--- class-clear-floats-button.php
+++ class-clear-floats-button.php
@@ -3,7 +3,7 @@
 Plugin Name: Clear Floats Button
 Plugin URI: https://github.com/Graffino/clear-floats-button
 Description: Adds a clear floats button to TinyMCE
-Version: 1.2.1
+Version: 1.2.2
 Author: Graffino
 Author URI: http://graffino.com
 
